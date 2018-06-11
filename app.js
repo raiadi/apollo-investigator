ab95155f0c4a05b603888da29882435a17cf9017
raiadi <thulung.a@hotmail.com>
2018-06-11 03:20:46 +0100

    updated routes for all the req

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-var bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    express     = require("express"),
-    app         = express();
-    
+var bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  express = require("express"),
+  app = express();
+
 app.use(express.static(__dirname + "/public"));
-    
-    app.get("/", function(req,res){
-        res.render("index.ejs");
-    });
-    
-    app.get("/services", function(req,res){
-        res.render("services.ejs");
-    });
-    
-    
-    
-app.listen(5001, function(req,res){
-    console.log("SERVER HAS STARTED");
+
+app.get("/", function(req, res) {
+  res.render("index.ejs");
+});
+
+app.get("/:id", function(req, res) {
+  var id = req.params.id;
+  res.render(id + ".ejs");
+});
+
+
+
+app.listen(5001, function(req, res) {
+  console.log("SERVER HAS STARTED");
 });
